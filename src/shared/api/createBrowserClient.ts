efa3d97900306ff8c60e4020ac85566757c27979
Hadi <postop09@naver.com>
2026-08-12 11:27:58 +0900

    setting: supabase 설정 수정

--- src/shared/api/createBrowserClient.ts
+++ src/shared/api/createBrowserClient.ts
@@ -3,6 +3,6 @@ import { createBrowserClient as createSupabaseBrowserClient } from '@supabase/ss
 export const createBrowserClient = () => {
   return createSupabaseBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
   );
 };
